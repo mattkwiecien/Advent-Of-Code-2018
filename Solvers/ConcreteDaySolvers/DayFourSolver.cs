66ad096014a04fa47ff0e9d5f35ed45abcc29924
Matthew Kwiecien <mkwieci2@gmail.com>
2018-12-26 11:12:27 -0600

    Refactored day 4 to use a new private Guard class.  This makes the code much more readable, and turns part 1 solver into 2 lines of code.

--- Solvers/ConcreteDaySolvers/DayFourSolver.cs
+++ Solvers/ConcreteDaySolvers/DayFourSolver.cs
@@ -4,61 +4,95 @@ using System.Text;
 using System.Linq;
 
 namespace AdventOfCode {
-	public class DayFourSolver : SolverBase {
+    public class DayFourSolver : SolverBase {
 
-		private List<GuardRecord> _guardRecords = new List<GuardRecord>();
+        private List<Guard> _guards = new List<Guard>();
 
-		public DayFourSolver(bool local) : base(4, local) {
-			var records = _myData.Split("\n");
+        public DayFourSolver(bool local) : base(4, local) {
+            var records = _myData.Split("\n");
+            var allRecords = CreateGuardRecords(records);
+            CreateGuardList(allRecords);
+        }
 
-			foreach (var record in records) {
-				var gRec = GuardRecord.FromRecord(record);
-				if (gRec == null) { continue; }
-				_guardRecords.Add(gRec);
-			}
-			_guardRecords = _guardRecords.OrderBy(x => x.Dt).ToList();
-			foreach(var rec in _guardRecords) {
-				System.Console.WriteLine($"{rec.Dt}: {rec.Desc}");
-			}
-		}
+        private void CreateGuardList(List<GuardRecord> allRecords) {
+            _guards = new List<Guard>();
 
-		protected override string PartOneSolver() {
-			var SleepJournal = new Dictionary<int, List<DateTime>>();
-			var currentGuardID = 0;
-			foreach (var gRecord in _guardRecords) {
-				if (gRecord.IsShiftRecord) {
-					currentGuardID = gRecord.GuardID;
-					if (!SleepJournal.ContainsKey(currentGuardID)) {
-						SleepJournal.Add(currentGuardID, new List<DateTime>());
-					}
-					continue;
-				}
-				SleepJournal[currentGuardID].Add(gRecord.Dt);
-			}
+            Guard currentGuard = null;
+            foreach (var gRecord in allRecords) {
+                if (!gRecord.IsShiftRecord && currentGuard != null) {
+                    currentGuard.SleepJournal.Add(gRecord);
+                    continue;
+                }
 
-			var MaxGuardSleep = new Dictionary<int, int>();
-			foreach (var kvp in SleepJournal) {
-				MaxGuardSleep.Add(kvp.Key, 0);
-				if (kvp.Value.Count == 0) { continue; }
+                var existingRecord = _guards.FirstOrDefault(x => x.GuardId == gRecord.GuardID);
+                if(existingRecord != null) {
+                    currentGuard = existingRecord;
+                } else {
+                    currentGuard = new Guard(gRecord.GuardID);
+                    _guards.Add(currentGuard);
+                }          
+            }
+        }
 
-				for (var i = 0; i < kvp.Value.Count; i = i + 2) {
-					var minutes = kvp.Value[i + 1] - kvp.Value[i];
-					MaxGuardSleep[kvp.Key] += int.Parse(minutes.TotalMinutes.ToString());
-				}
-			}
-			var sleeper = MaxGuardSleep.OrderByDescending(x => x.Value).ToList().First();
-			var sleepSchedule = SleepJournal[sleeper.Key];
+        private List<GuardRecord> CreateGuardRecords(string[] records) {
+            var allRecords = new List<GuardRecord>();
+            foreach (var record in records) {
+                var gRec = GuardRecord.FromRecord(record);
+                if (gRec == null) { continue; }
+                allRecords.Add(gRec);
+            }
+            return allRecords.OrderBy(x => x.Dt).ToList();
+        }
 
-			var minuteCount = new Dictionary<int, int>();
-			for (var j = 0; j < sleepSchedule.Count; j = j + 2){
-				//for
-			}
-			return "";		
-		}
+        protected override string PartOneSolver() {
+            var sleeper = _guards.OrderByDescending(x => x.TotalMinutesSlept).FirstOrDefault();
+            return (sleeper.GuardId * sleeper.MinuteMostAsleep).ToString();
+        }
 
-		protected override string PartTwoSolver() {
-			return "";
-		}
+        protected override string PartTwoSolver() {
+            return "";
+        }
+
+        private class Guard {
+            public int GuardId { get; set; }
+            public List<GuardRecord> SleepJournal { get; set; }
+            public int TotalMinutesSlept => GetMinutesAsleep();
+            public int MinuteMostAsleep => GetMinuteMostAsleep();
+
+            public Guard(int guardId) {
+                this.GuardId = guardId;
+                SleepJournal = new List<GuardRecord>();
+            }
+
+            private int GetMinutesAsleep() {
+                var cnt = 0;
+                if (SleepJournal.Count == 0) { return cnt; }
+
+                for (var i = 0; i < SleepJournal.Count; i = i + 2) {
+                    var span = SleepJournal[i + 1].Dt - SleepJournal[i].Dt;
+                    cnt += int.Parse(span.TotalMinutes.ToString());
+                }
+
+                return cnt;
+            }
+
+            private int GetMinuteMostAsleep() {
+                var minuteCount = new Dictionary<int, int>();
+
+                for (var j = 0; j < this.SleepJournal.Count; j = j + 2) {
+                    var span = (this.SleepJournal[j + 1].Dt - this.SleepJournal[j].Dt).TotalMinutes;
+                    for (var i = this.SleepJournal[j].Dt.Minute; i < this.SleepJournal[j].Dt.Minute + span; i++) {
+                        if (minuteCount.ContainsKey(i)) {
+                            minuteCount[i]++;
+                        } else {
+                            minuteCount.Add(i, 1);
+                        }
+                    }
+                }
+
+                return minuteCount.OrderByDescending(x => x.Value).FirstOrDefault().Key;
+            }
+        }
 
 		private partial class GuardRecord {
 			public DateTime Dt { get; set; }
