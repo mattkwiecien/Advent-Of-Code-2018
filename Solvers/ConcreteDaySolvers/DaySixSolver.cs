f5b4423a57582b5dbfd68f2979c247a1d79a43fe
Matthew Kwiecien <mkwieci2@gmail.com>
2018-12-31 11:15:57 -0600

    Small refactor to part one.

--- Solvers/ConcreteDaySolvers/DaySixSolver.cs
+++ Solvers/ConcreteDaySolvers/DaySixSolver.cs
@@ -7,47 +7,36 @@ namespace AdventOfCode {
     public class DaySixSolver : SolverBase {
 
         private List<(int, int)> vectors = new List<(int, int)>();
-        private Dictionary<string, int> boundaries = new Dictionary<string, int>();
 
         public DaySixSolver(bool local) : base(6, local) {
             GetVectorsFromInput();
         }
 
         protected override string PartOneSolver() {
-            RemoveInfinitePoints();
-            var width = 1000;
-            var height = 1000;
 
-            var grid = new int[width, height];
-            var valueGrid = new int[width, height];
-            for (var x = 0; x < width; x++) {
-                for (var y = 0; y < height; y++) {
-                    grid[x, y] = -1;
-                    valueGrid[x, y] = 100000;
-                }
-            }
+            int width = 1000, height = 1000;
+            //Grid used to mark each location with a unique ID per vector (index of the vector in the master vector array)
+            var grid = GetMatrix(width, height, -1);
+            //Grid used to mark each location with the smallest taxicab distance
+            var distanceGrid = GetMatrix(width, height, GetTaxicabDistance((0, 0), (width, height)));
 
-            foreach (var vector in vectors) {
-                for (var x = 0; x < width; x++) {
-                    for (var y = 0; y < height; y++) {
-                        var distToPoint = GetTaxicabDistance(vector, (y, x));
+            SetGridPoints(ref grid, ref distanceGrid, width, height);
+            var TaxicabDistancesByVector = GetDistancesFromGrid(grid);
+            return TaxicabDistancesByVector.Values.Max().ToString();
 
-                        if (vector.Equals((y,x))) {
-                            valueGrid[x, y] = -1;
-                            grid[x, y] = vectors.IndexOf(vector);
-                        } else if (distToPoint == valueGrid[x, y]) {
-                            valueGrid[x, y] = distToPoint;
-                            grid[x, y] = -1;
-                        } else if (distToPoint < valueGrid[x, y]) {
-                            valueGrid[x, y] = distToPoint;
-                            grid[x, y] = vectors.IndexOf(vector);
-                        }
-                    }
-                }
-            }
+        }
 
-            //displayGrid(grid, width, height);
+        protected override string PartTwoSolver() {
+            return "";
+        }
+
+        private int GetTaxicabDistance((int, int) p, (int, int) q) {
+            return Math.Abs(p.Item1 - q.Item1) + Math.Abs(p.Item2 - q.Item2);
+        }
+
+        private Dictionary<int, int> GetDistancesFromGrid(int[,] grid) {
             var counts = new Dictionary<int, int>();
+            var boundaries = GetBounds();
             for (var x = boundaries["xstart"]; x <= boundaries["xend"]; x++) {
                 for (var y = boundaries["ystart"]; y <= boundaries["yend"]; y++) {
                     if (counts.ContainsKey(grid[x, y])) {
@@ -57,42 +46,32 @@ namespace AdventOfCode {
                     }
                 }
             }
-
-
-            return counts.Values.Max().ToString();
+            return counts;
         }
-        private void displayGrid(string[,] grid, int width, int height) {
-            for (var x = 0; x < width; x++) {
-                for (var y = 0; y < height; y++) {
-                    Console.Write(grid[x, y]);
+
+        private void SetGridPoints(ref int[,] grid, ref int[,] distanceGrid, int width, int height) {
+            foreach (var vector in vectors) {
+                for (var x = 0; x < width; x++) {
+                    for (var y = 0; y < height; y++) {
+                        var distToPoint = GetTaxicabDistance(vector, (y, x));
+
+                        if (vector.Equals((y, x))) {
+                            distanceGrid[x, y] = -1;
+                            grid[x, y] = vectors.IndexOf(vector);
+                        } else if (distToPoint == distanceGrid[x, y]) {
+                            distanceGrid[x, y] = distToPoint;
+                            grid[x, y] = -1;
+                        } else if (distToPoint < distanceGrid[x, y]) {
+                            distanceGrid[x, y] = distToPoint;
+                            grid[x, y] = vectors.IndexOf(vector);
+                        }
+                    }
                 }
-                Console.Write('\n');
             }
         }
 
-        protected override string PartTwoSolver() {
-            return "";
-        }
-
-        private string getLetterFromVector((int,int) input) {
-            if (input.Equals((1, 1))) {
-                return "a";
-            } else if (input.Equals((1, 6))) {
-                return "b";
-            } else if (input.Equals((8, 3))) {
-                return "c";
-            } else if (input.Equals((3, 4))) {
-                return "d";
-            } else if (input.Equals((5, 5))) {
-                return "e";
-            } else if (input.Equals((8, 9))) {
-                return "f";
-            } else {
-                return "x";
-            }
-        }
-
-        private void RemoveInfinitePoints() {
+        private Dictionary<string, int> GetBounds() {
+            var boundaries = new Dictionary<string, int>();
             foreach (var xy in vectors) {
                 var xless = vectors.FirstOrDefault(x => x.Item1 < xy.Item1);
                 if (xless.Equals(default) && !boundaries.ContainsKey("xstart")) {
@@ -111,19 +90,24 @@ namespace AdventOfCode {
                     boundaries.Add("yend", xy.Item2);
                 }
             }
+            return boundaries;
         }
 
         private void GetVectorsFromInput() {
-            //var testData = @"1, 1.1, 6.8, 3.3, 4.5, 5.8, 9";
-            //foreach (var xy in testData.Split('.')) {
             foreach (var xy in _myData.Split('\n')) {
                 if (xy.Split(',').Length < 2) { continue; }
                 vectors.Add((int.Parse(xy.Split(',')[0].Trim()), int.Parse(xy.Split(',')[1].Trim())));
             }
         }
 
-        private int GetTaxicabDistance((int, int) p, (int, int) q) {
-            return Math.Abs(p.Item1 - q.Item1) + Math.Abs(p.Item2 - q.Item2);
+        private int[,] GetMatrix(int width, int height, int defaultVal) {
+            var mtx = new int[width, height];
+            for (var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    mtx[x, y] = defaultVal;
+                }
+            }
+            return mtx;
         }
 
     }
